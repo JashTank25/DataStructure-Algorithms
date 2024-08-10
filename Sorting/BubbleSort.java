efceef5d8c1bc0c4e16c5a67b16f8c400f30d164
Rashi2006 <140819546+Rashi2006@users.noreply.github.com>
2024-08-10 11:20:52 +0530

    Sorting

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -16,7 +16,7 @@ public class BubbleSort{
     int swap;
     for(int i=0;i<(n-1);i++){
     	isFound=false;
-        for(int j=0;j<(n-1);j++){
+        for(int j=0;j<(n-i-1);j++){
             if(arr[j]>arr[j+1]){
                swap=arr[j];
                arr[j]=arr[j+1];
